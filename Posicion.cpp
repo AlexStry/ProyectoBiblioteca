c3f783c7547e316bda5fbfbe82c958f83bf09622
AlexStry <150042828+AlexStry@users.noreply.github.com>
2024-05-23 07:14:39 -0600

    Create Posicion.cpp

---
+++ Posicion.cpp
@@ -0,0 +1,17 @@
+#include "Posicion.h"
+//Metodo par obtener y devolver la coordenada de x
+int Posicion::getX() const {
+    return x;
+}
+//Metodo para establecer la coordenada de x
+void Posicion::setX(int x) {
+    this->x = x;
+}
+//Metodo para obtener y devolver la coordeanada de y
+int Posicion::getY() const {
+    return y;
+}
+//Metodo para establecer la coordenada de y
+void Posicion::setY(int y) {
+    this->y = y;
+}
