aedef645180ab95e6b685d03336f3f27fc03e83f
AlexStry <150042828+AlexStry@users.noreply.github.com>
2024-05-23 07:14:21 -0600

    Create Objeto.h

---
+++ Objeto.h
@@ -0,0 +1,13 @@
+#ifndef OBJETO_H
+#define OBJETO_H
+#include "Posicion.h"
+
+class Objeto {
+    protected:
+    Posicion posicion;
+    //variables
+
+    public:
+    Posicion getPosicion() const; //Obtener la posicion de un objeto
+    void setPosicion(const Posicion& p); //establecer la posicion del objeto
+};
