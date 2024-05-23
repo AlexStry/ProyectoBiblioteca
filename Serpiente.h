c01e1e3382e3160c256dbc5841ae7a820aefaf4c
AlexStry <150042828+AlexStry@users.noreply.github.com>
2024-05-23 07:29:40 -0600

    Update Serpiente.h

--- Serpiente.h
+++ Serpiente.h
@@ -10,6 +10,8 @@ class Serpiente : public Objeto {
     string direccion;
     //variables
     public:
+    Serpiente(); //Constructor
+    Serpiente(int tamaño, const string& direccion);
     void mover(); //Movimiento de la serpiente
     void aumentarTamaño(); //Aumento de taamño de la serpiente
     void cambiarDireccion(const string& nuevaDireccion); //Metodo para cambiar la direccion de la serpiente
