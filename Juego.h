f4900fbc27ad7e1d403343a2250fa7a6a9e89c5f
AlexStry <150042828+AlexStry@users.noreply.github.com>
2024-05-23 07:14:08 -0600

    Create Juego.h

---
+++ Juego.h
@@ -0,0 +1,17 @@
+#ifndef JUEGO_H
+#define JUEGO_H
+#include "Tablero.h"
+#include "Serpiente.h"
+#include "Comida.h"
+
+class Juego {
+    private:
+    Tablero tablero;
+    Serpiente serpiente;
+    Comida comida;
+    //varibles
+
+    public:
+    void empezarJuego(); //Comenzar el juego
+    void terminarJuego(); //Terminar el juego
+};
