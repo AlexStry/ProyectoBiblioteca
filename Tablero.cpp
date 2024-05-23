6493155b0766ebaf2c694e968a6e75343dd4dc08
AlexStry <150042828+AlexStry@users.noreply.github.com>
2024-05-23 07:29:55 -0600

    Update Tablero.cpp

--- Tablero.cpp
+++ Tablero.cpp
@@ -1,5 +1,11 @@
 #include "Tablero.h"
 
+//Constructores
+Tablero::Tablero() : ancho(0), altura(0), serpiente() {}
+
+Tablero::Tablero(int ancho, int altura, const Serpiente& serpiente)
+    : ancho(ancho), altura(altura), serpiente(serpiente) {}
+
 void Tablero::dibujar() {
     //Creacion del tablero
 }
