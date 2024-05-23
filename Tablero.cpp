d4e2efd5685cf5feaf2b25ae360617fc6c90e2ed
AlexStry <150042828+AlexStry@users.noreply.github.com>
2024-05-23 07:16:15 -0600

    Create Tablero.cpp

---
+++ Tablero.cpp
@@ -0,0 +1,9 @@
+#include "Tablero.h"
+
+void Tablero::dibujar() {
+    //Creacion del tablero
+}
+
+bool Tablero::checarColision(const Posicion& p) {
+    //Aqui se implementa el metodo de colision
+}
