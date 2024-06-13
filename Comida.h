d0b8a79459aa91382b30df4fe80c8167982b5ade
AlexStry <150042828+AlexStry@users.noreply.github.com>
2024-06-12 22:50:17 -0600

    Delete Comida.h

--- Comida.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMIDA_H
-#define COMIDA_H
-#include "Objeto.h"
-
-class Comida : public Objeto {
-    public:
-    Comida(); //Constructor
-    void revivir(); //Metodo para crear mas comida
-};
